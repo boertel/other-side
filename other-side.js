ae2295c4c7d5c6c9dea2ac83b531c22e4b522118
boertel <benjamin.oertel@gmail.com>
2016-07-28 16:20:30 -0700

    radius around the office"

--- other-side.js
+++ other-side.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/boertel/ciqy9y3fl0001bpnohs9hdps8',
     center: [-122.40836709737778, 37.79942516278797],
-    zoom: 16,
+    zoom: 15,
 });
 
 function projectPoint(lon, lat) {
@@ -38,18 +38,46 @@ map.on('load', function() {
         }
     });
 
-    /*
-    window.setTimeout(function() {
-        var bounds = new mapboxgl.LngLatBounds();
-        var features = map.queryRenderedFeatures({layers: ['point']});
-        if (features.length) {
-            features.forEach(function(feature) {
-                bounds.extend(feature.geometry.coordinates);
-            });
-            map.fitBounds(bounds, {padding: 200});
+    var office = [-122.40603893995284, 37.797831373796456];
+
+    map.addSource('radius', {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: [
+                {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: office,
+                    }
+                }
+            ]
         }
-    }, 1000);
-    */
+    });
+
+    var RADIUS = 700;
+
+    function metersToPixelsAtMaxZoom(meters, latitude) {
+        return meters / 0.075 / Math.cos(latitude * Math.PI / 180)
+    }
+
+    map.addLayer({
+        id: 'radius',
+        type: 'circle',
+        source: 'radius',
+        paint: {
+            'circle-color': 'rgba(0, 0, 0, 0.1)',
+            'circle-radius': {
+                stops: [
+                    [0, 0],
+                    [20, metersToPixelsAtMaxZoom(700, office[1])]
+                ],
+                base: 2
+            }
+
+        }
+    });
 
 });
 
